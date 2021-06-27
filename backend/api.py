a414e7620eee999e94b68810a37891fee23790cb
Breenda Das <breenda.das@gmail.com>
2021-06-27 12:40:48 +0530

    Update api.py

--- backend/api.py
+++ backend/api.py
@@ -14,7 +14,8 @@ def convert_to_json(csv_path = './csv/hospital.csv', json_path='./json/hospital.
             reader = csv.DictReader(csvfile, fieldnames)
             final_data = {}
             for row in reader:
-                final_data[row["Date"]] = {
+                final_data = [{
+                    "Date" : row['Date'],
                     "Total_new_patients": row["Total_new_patients"],
                     "Total_patients_died": row["Total_patients_died"],
                     "Total_patients_cured": row["Total_patients_cured"],
@@ -26,6 +27,7 @@ def convert_to_json(csv_path = './csv/hospital.csv', json_path='./json/hospital.
                     "Total_oxygen_supply_tonnes": row["Total_oxygen_supply_tonnes"],
                     "Total_bed_supply": row["Total_bed_supply"],
                     "Total_vaccination_doses_available": row["Total_vaccination_doses_available"],
-                }
+                }]
             json.dump(final_data, jsonfile)
             jsonfile.write('\n')
+convert_to_json(csv_path = './csv/hospital.csv', json_path='./json/hospital.json')
