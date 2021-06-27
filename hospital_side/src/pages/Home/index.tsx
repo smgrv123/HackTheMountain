53c256d88b19b43915a13e89b3961cfba68c518e
shubhraagarwal <agarwalshubhra2002@gmail.com>
2021-06-27 08:15:44 +0530

    :construction: Added patient list
    Currently the list is not working

--- hospital_side/src/pages/Home/index.tsx
+++ hospital_side/src/pages/Home/index.tsx
@@ -1,10 +1,12 @@
 import React, { ReactElement } from 'react';
+import PatientList from '../../components/Home/PatientList';
 import { PatientModal } from '../../components/Home/PatientModal';
 
 export default function Home(): ReactElement {
   return (
     <div>
       <PatientModal />
+      <PatientList />
     </div>
   );
 }
